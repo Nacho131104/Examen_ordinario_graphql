27b0357f4ac39c1e42e0256c8eea20fa4af34201
Nacho131104 <nacholopezamat@gmail.com>
2025-01-31 16:17:06 +0100

    update

--- resolvers.ts
+++ resolvers.ts
@@ -9,12 +9,22 @@ type context ={
 type getRestaurantArgs={
     id: string,
 }
+
+type getRestaurantsArgs={
+    name: string,
+}
 export const resolvers ={
     Query:{
         getRestaurant:async(_:unknown, args:getRestaurantArgs,ctx:context): Promise<RestaurantModel|null> =>{
             const restauranteEncontrado = await ctx.restaurantsCollection.findOne({_id:new ObjectId(args.id)});
             if(!restauranteEncontrado)throw new GraphQLError("Restaurante no encontrado");
             return restauranteEncontrado;
+        },
+
+        getRestaurants:async(_:unknown,args:getRestaurantsArgs,ctx:context):Promise<RestaurantModel[]|null> =>{
+            const restaurantes = await ctx.restaurantsCollection.find({name:args.name}).toArray();
+            if(!restaurantes)throw new GraphQLError("No se encontraron restaurantes con ese nombre");
+            return restaurantes;
         }
     },
 
