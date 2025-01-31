dd7f50458fe52bae84d4f0f5bb64de991a3b0df4
Nacho131104 <nacholopezamat@gmail.com>
2025-01-31 12:14:53 +0100

    update

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
-//import {MongoClient} from "mongodb";
+import {MongoClient} from "mongodb";
 import {ApolloServer} from "@apollo/server";
 import { schema } from "./schema.ts";
 import {startStandaloneServer} from "@apollo/server/standalone";
 import {resolvers} from "./resolvers.ts";
 
-/*
+
 const MONGO_URL = Deno.env.get("MONGO_URL");
 
 if (!MONGO_URL) {
@@ -17,9 +17,9 @@ await mongoClient.connect();
 console.info("Connected to MongoDB");
 
 const mongoDB = mongoClient.db("ciudades");
-const CityCollection = mongoDB.collection("ciudad");
 
-*/
+
+
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
